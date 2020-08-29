8ca74c5f458d3e0493e34132fc45c38c033bc3d5
QuinlanVoz <69868257+QuinlanVoz@users.noreply.github.com>
2020-08-28 21:53:30 -0500

    Luis

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   #resources :proyectos
   #get 'home/index'
  
+  get 'home/index'
   get 'static/nosotros'
   get 'static/contactenos'
   get 'static/acceder'
@@ -13,7 +14,7 @@ Rails.application.routes.draw do
   root 'home#index'
 
   resources :proyectos
-  get 'proyectos/proyecto_index'
+  get 'proyectos/index'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
