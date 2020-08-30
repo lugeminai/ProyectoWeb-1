1a6dcc4b946e412e501c68963e0cc7f9500edda3
QuinlanVoz <69868257+QuinlanVoz@users.noreply.github.com>
2020-08-30 02:09:26 -0500

    cabecera

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,40 @@
+class AdminController < ApplicationController
+	layout 'admin'
+
+	def admDepartamento		
+	end
+
+	def ingDepartamento		
+	end
+
+	def admProyecto	
+	end
+
+	def ingProyecto		
+	end
+
+	def admUsuario
+	end
+
+	def ingUsuario	
+	end
+
+	def solicitudcita
+	end
+
+	def separardepartamento
+	end
+
+	def detalledep
+	end 
+	
+	def reporte
+	end
+
+	def ventas
+	end
+
+	def ingVentas
+	end
+  
+end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,6 @@ class HomeController < ApplicationController
   def index
   end
 
-  def proyectoindex
-    render :index
-  end
-
   def index2
   end
 
