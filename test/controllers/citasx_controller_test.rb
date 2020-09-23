f02b3ffbf63d0da1d9498ecec6af9f138a09d4cb
Mirian <lugemi@gmail.com>
2020-09-23 12:06:08 -0500

    Delete citasx_controller_test.rb

--- test/controllers/citasx_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class CitasControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get citas_index_url
-    assert_response :success
-  end
-
-end
