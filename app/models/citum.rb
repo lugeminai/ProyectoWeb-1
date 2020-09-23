503a6b3f1db20c10304443c93d9c40f9623efaf0
Mirian <lugemi@gmail.com>
2020-09-23 12:05:57 -0500

    Update citum.rb

--- app/models/citum.rb
+++ app/models/citum.rb
@@ -1,2 +1,21 @@
 class Citum < ApplicationRecord
+    validates :codigo, presence: {
+        message: "es requerido"
+    }
+    validates :nombre, presence: {
+        message: "es requerido"
+    }    
+    validates :apellido, presence: {
+        message: "es requerido"
+    }
+    validates :dni, presence: {
+        message: "es requerido"
+    }
+    validates :telefono, presence: {
+        message: "es requerido"
+    }
+    validates :fecha, presence: {
+        message: "es requerido"
+    }    
+    
 end
